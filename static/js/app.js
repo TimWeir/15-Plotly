82d10cac05ae966d016eb37f41a194d1e339948d
Tim Weir <timothy.weir@comcast.net>
2021-05-14 21:05:10 -0400

    more progress

--- static/js/app.js
+++ static/js/app.js
@@ -3,54 +3,38 @@
 var subjectIndex = 0;
 
 
+
+
+
+function optionChanged(subjectIndex) {
+    buildPlot(subjectIndex);
+    demoInfo(subjectIndex);
+}
+
 /*
-// Use D3 to select the dropdown menu
-var dropdownMenu = d3.select("#selDataset");
-// Assign the value of the dropdown menu option to a variable
-var dataset = dropdownMenu.property("value");
+d3.selectAll("#selDataset").on("change", updatePage);
+// Function to handle input change
+function updatePage() {
+    // grab the value of the input field
+    var dropMenu = d3.selectAll("#selDataset").node();
+    
+    //selectIndex = dropMenu.text;
+    //console.log(selectIndex);
+
+    var subjectIndex = dropMenu.value;
+    console.log(subjectIndex);
+    return subjectIndex;
+    
+    //buildPlot();
+    //demoInfo();
+};
 */
 
 
-// grab subject ID from dropdown menu
-
-function popDropDown() {
+function buildPlot(patient) {
     d3.json("samples.json").then(function(data){
-        var population = data.names
-        console.log(population)
-        //var selectList = d3.select("#selDataset");
-        population.forEach(function(patient){
-            d3.select("#selDataset")
-                .append("option")
-                .text(patient);
-        });
-    });
-};
-
-
-
-d3.selectAll(".well").on("change", updatePage);
-// Function to handle input change
-function updatePage() {
-    
-    // grab the value of the input field
-    var dropMenu = d3.selectAll("#selDataset").node();
-
-    subjectIndex = dropMenu.id;
-    console.log(subjectIndex);
-
-    var dropMenuSelected = dropMenu.value;
-    console.log(dropMenuSelected);
-
-    updatePage.on("change", buildPlot);
-    updatePage.on("change", demoInfo);
-};
-
-
-
-function buildPlot() {
-    d3.json("samples.json").then(function(data){
-        // console.log(data)
-        var graphData = data.samples[subjectIndex];
+        var patientData = data.samples.filter(object => object.id == patient);
+        var graphData = patientData[0];
         var values = graphData.sample_values.slice(0,10).reverse();
         var labels = graphData.otu_ids.slice(0,10).reverse();
         var hovertext =  graphData.otu_labels.slice(0,10).reverse();
@@ -71,7 +55,7 @@ function buildPlot() {
         data = [trace1];
 
         layout = {
-            title: "First 10 OTUs"
+            title: `First 10 OTUs of ${patient}`
         }
 
         Plotly.newPlot("bar", data, layout);
@@ -96,7 +80,7 @@ function buildPlot() {
             data = [trace2];
     
             layout = {
-                title: "OTU ID Bubble Plot"
+                title: `OTU ID Bubble Plot of ${patient}`
                 //"position": "bottom"
             }
     
@@ -106,22 +90,45 @@ function buildPlot() {
 };
 
 
-function demoInfo() {
+function demoInfo(patient) {
     d3.json("samples.json").then(function(data){
-        var panel = d3.select(".panel-body");
-        var panelrow = panel.append("p").text(`ID: ${data.metadata[subjectIndex].id}`);
-        var panelrow = panel.append("p").text(`Age: ${data.metadata[subjectIndex].age}`);
-        var panelrow = panel.append("p").text(`Ethnicity: ${data.metadata[subjectIndex].ethnicity}`);
-        var panelrow = panel.append("p").text(`Gender: ${data.metadata[subjectIndex].gender}`);
-        var panelrow = panel.append("p").text(`Location: ${data.metadata[subjectIndex].location}`);
-        var panelrow = panel.append("p").text(`wfreq: ${data.metadata[subjectIndex].wfreq}`);
-    })
+        var panel = d3.select("#sample-metadata");
+        panel.selectAll("p")
+            .data(data)
+            .exit()
+            .remove();
+        var filterInfo = data.metadata.filter(object => object.id == patient);
+        var info = filterInfo[0];
+        Object.entries(info).forEach(([k,v]) => {
+            panel.append("p").text(`${k}: ${v}`);
+        });
+    });
 };
 
 
+//buildPlot();
+//demoInfo();
 
-buildPlot();
+function init() {
+    d3.json("samples.json").then(function(data){
+        var population = data.names
+        //console.log(population);
+        population.forEach(function(patient){
+            d3.select("#selDataset")
+                .append("option")
+                .property("value", patient)
+                .text(patient);
+        });
+        var patientZero = population[0];
+        buildPlot(patientZero);
+        demoInfo(patientZero)
+    });
+};
 
-popDropDown();
+function optionChanged(patient) {
+    buildPlot(patient);
+    demoInfo(patient);
+    //console.log(patient)
+}
 
-demoInfo();
+init();
